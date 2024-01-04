8bebc35abbf2e69383609468e3132196bb07cc1b
kabakae <kabakaevans@gmail.com>
2024-01-04 04:33:19 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
-    alif = dir()
-    for i in range(0, len(alif)):
+    allf = dir()
+    for i in range(0, len(allf)):
         if allf[i][:2] != "__":
             print("(:s)".format(allf[i]))
