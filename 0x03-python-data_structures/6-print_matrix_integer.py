1aefa5830f6af0d82d492ed91a1e6c83f62762f5
kabakae <kabakaevans@gmail.com>
2024-01-08 10:04:19 -0800

    4-new_in_list.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,11 +5,9 @@ def print_matrix_integer(matrix=[[]]):
     else:
         for row in range(len(matrix)):
             for item in range(len(matrix[row])):
-                if item != len(matrix[row])- 1:
+                if item != len(matrix[row]) - 1:
                     endspace = ' '
                 else:
                     endspace = ''
                 print("{:d}".format(matrix[row][item]), end=endspace)
             print()
-
-
