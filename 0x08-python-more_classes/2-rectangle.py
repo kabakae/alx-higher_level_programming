47740bcdfb28caedada12a09a6fa557a27cd32ac
kabakae <kabakaevans@gmail.com>
2024-01-31 00:25:27 -0800

    003

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 """Define a Rectangle Class."""
 
 
-class Rectagle:
+class Rectangle:
     """Represent a rectagle."""
 
     def __init__(self, width=0, height=0):
@@ -18,8 +17,8 @@ class Rectagle:
 
     @property
     def width(self):
-            """Get/set the width of the Rectagle."""
-            return self.__width
+        """Get/set the width of the Rectagle."""
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -51,18 +50,3 @@ class Rectagle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-
-    def __str__(self):
-       """Return the printable representation of the Rectangle.
-       Represents the rectangle with the # character.
-       """
-
-       if self.__width == 0 or self.__height == 0:
-           return ("")
-
-       rect = []
-       for i in range(self.__height):
-           [rect.append('#')for j in range(self.__width)]
-           if i != self.__height - 1:
-               rect.append("\n")
-       return ("".join(rect))
