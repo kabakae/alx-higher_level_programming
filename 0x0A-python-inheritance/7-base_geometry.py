3a2fcf9e1d9c315e12be8778d36e9915eecc978f
kabakae <kabakaevans@gmail.com>
2024-02-05 06:40:18 -0800

    0007

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines base geometry."""
+
+
+class BaseGeometry:
+    """Represent."""
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implement")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter.
+        Args:
+        Raises:
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
