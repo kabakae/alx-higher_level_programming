9566eaa3ec05343a2892d3eb34686267b446fcd2
kabakae <kabakaevans@gmail.com>
2024-02-06 09:56:21 -0800

    0010

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+
+    def __init__(self, first_name, last_name, age):
+        """Intilialize a new Student.
+
+        Args:
+            first_name
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictonary of the student.
+        Args:
+            attrs (list): (Optional)
+        """
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
