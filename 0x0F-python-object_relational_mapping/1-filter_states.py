52572b024eb1f49f84dbf85c6b755c7273fdb592
kabakae <kabakaevans@gmail.com>
2024-04-12 01:27:17 -0700

    0000

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that lists all states from the hbtn_Oe_O_usa database."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    #Get MySQL credentials
+    #Connect to my MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+
+    # Execute the SQL query to retrieve all states sorted by i.d
+    c.execute("SELECT * FROM states ORDER BY id ")
+    [print(states) for state in c.fetchall() if state[1][0] == "N"]
