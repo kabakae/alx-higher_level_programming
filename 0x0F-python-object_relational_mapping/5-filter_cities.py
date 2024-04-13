8181e842cf894e5b54cb092c408b001ea7cb3719
kabakae <kabakaevans@gmail.com>
2024-04-13 04:22:05 -0700

    0000

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     # Execute the SQL query
     query = ("SELECT * FROM cities as c \
             INNER JOIN states as s \
-            ON c . state_id = s . id \
-            ORDER BY c .id ")
+            ON c.state_id = s.id \
+            ORDER BY c.id ")
     c.execute(query)
     # Fetch all rows
     print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
