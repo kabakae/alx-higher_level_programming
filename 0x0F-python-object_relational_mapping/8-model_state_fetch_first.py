63a6eb61f0752a4484139809f29131f26f30f08d
kabakae <kabakaevans@gmail.com>
2024-04-13 09:44:16 -0700

    000

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,14 +8,14 @@ from model_state import State
 if __name__ == "__main__":
     # Create the SQLALchemy
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                        pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     # Create a session factory
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    #Retrieve all states from
+    # Retrieve all states from
     state = session.query(State).order_by(State.id).first()
     if state is None:
         print("Nothing")
