de024d451c7c7970329c4c2e966b2c01d0cad94d
kabakae <kabakaevans@gmail.com>
2024-04-13 07:41:57 -0700

    0000

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://():()@localhost/()"
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     # Create a session factory
+    Session = sessionmaker(bind=engine)
+
     session = Session()
 
     #Retrieve all states from
