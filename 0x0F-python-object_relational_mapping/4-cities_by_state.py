35574746d5927674dc94affade3d68255c840f83
kabakae <kabakaevans@gmail.com>
2024-04-13 03:08:03 -0700

    0000

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that lists all states."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Get MySQL credentials and search
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # connect to MySQL server
+    c = db.cursor()
+
+    # Execute the SQL 
+    c.execute("SELECT 'c', 'id', 'c', 'name' \
+            FROM 'cities' as 'c' \
+            INNER JOIN 'states' as 's' \
+                ON 'c', 'states_id' = 's', 'id' \
+            ORDER BY 'c', 'id'")
+    # Fetch all rows and print the states
+    [print(city) for city in c.fetchall()]
