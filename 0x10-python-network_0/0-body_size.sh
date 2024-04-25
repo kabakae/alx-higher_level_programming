7e641cef751a7a33485343c681ecd6610a7936c8
kabakae <kabakaevans@gmail.com>
2024-04-25 10:31:48 -0700

    0000

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Bash script that takes in a URL, sends a request to that URL, and displays the size of the body
+
+# Check if the URL is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a request to the URL using curl and get the size of the response body
+response=$(curl -s -w "%{size_download}" -o /dev/null "$1")
+
+# Display the size of the response body in bytes
+echo "$response"
