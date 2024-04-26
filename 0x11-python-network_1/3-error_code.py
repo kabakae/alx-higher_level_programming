34bdce19ca4f192de5a7e1049510cb60e59a4b6a
kabakae <kabakaevans@gmail.com>
2024-04-26 13:59:03 -0700

    0000

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Sends a request to the provided URL and displays the body of the response (decoded in utf-8).
+
+This script takes in a URL as a command-line argument. It then sends a request using urllib.request.urlopen() to the provided URL and displays the body of the response decoded in utf-8. If an HTTPError occurs, it prints the error code.
+
+Example:
+    $ python3 script_name.py https://example.com
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: {} <URL>".format(sys.argv[0]))
+    sys.exit(1)
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+        print(body)
+except urllib.error.HTTPError as e:
+    print("Error code:", e.code)
