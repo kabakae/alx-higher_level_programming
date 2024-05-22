ff612ff44bd06cf602ce9df89febdddb101efae1
kabakae <evanskabaka@gmail.com>
2024-05-22 06:40:37 -0700

    00

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+/*
+This script prints the number of movies where the character "Wedge Antilles" is present.
+The first argument is the API URL of the Star Wars API: https://swapi-api.alx-tools.com/api/films/
+Wedge Antilles is character ID 18 - your script must use this ID for filtering the result of the API
+You must use the module request
+*/
+
+const request = require('request');
+const process = require('process');
+
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+if (!apiUrl) {
+  console.error('Usage: ./4-starwars_count.js <API_URL>');
+  process.exit(1);
+}
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Error: Status code ${response.statusCode}`);
+    return;
+  }
+  const films = JSON.parse(body).results;
+  const filmsWithWedgeAntilles = films.filter(film =>
+    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+  );
+  console.log(filmsWithWedgeAntilles.length);
+});
